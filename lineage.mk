b3e2662674efc78e034ad63478208fc2772d1535
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-01-01 19:52:30 +0100

    Z00L: Lineage branding
    Change-Id: Ib5aabf759b1619027943e4efdf234b2460d9b184

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/asus/Z00L/full_Z00L.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_Z00L
+PRODUCT_NAME := lineage_Z00L
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00L
