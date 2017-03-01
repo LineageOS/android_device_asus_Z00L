663ccaf7a0956e27fc9ec7048cdd0fc36727c719
LuK1337 <priv.luk@gmail.com>
2017-03-01 13:58:14 +0100

    Z00L: Set initial copyright year
    Change-Id: I4c54af9c2407c231273803ebacaff0452946d32f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +17,8 @@
 #
 
 set -e
+
+export INITIAL_COPYRIGHT_YEAR=2016
 
 # Required!
 export DEVICE=Z00L
